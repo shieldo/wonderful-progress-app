0bc0db3b528de4bfb0404e6cb498af6def691ee6
Douglas Greenshields <dgreenshields@gmail.com>
2021-08-14 22:47:13 +0100

    create initial nuxt app that fetches and shows data from the netlify function

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Word count is {{ wc }} out of a total of {{ totalWords }}</h1>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      wc: 0,
+      totalWords: 0,
+    };
+  },
+  async created() {
+    let res;
+    try {
+      res = await axios.get("https://wonderful-progress-ahead.netlify.app/.netlify/functions/progress");
+    } catch (err) {
+      // handle error
+      return;
+    }
+    this.wc = res.data.wc;
+    this.totalWords = res.data.totalWords;
+  }
+}
+</script>
